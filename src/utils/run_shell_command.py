84f7dbf8ea07b6826f13630bee7e66d56a3de43a
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-18 17:14:08 +0100

    Add stderr to `run_shell_command`

--- src/utils/run_shell_command.py
+++ src/utils/run_shell_command.py
@@ -4,5 +4,7 @@ from typing import Optional
 
 def run_shell_command(command: str, cwd: Optional[str] = None) -> str:
     process = subprocess.run(command.split(" "), capture_output=True, cwd=cwd)
-    assert process.returncode == 0, f'command "{command}" failed'
+    assert (
+        process.returncode == 0
+    ), f'command "{command}" failed: {process.stderr.decode()}'
     return process.stdout.decode().strip()
